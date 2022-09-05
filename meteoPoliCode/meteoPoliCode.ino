3e0c4aa28503e3e4a7e1555ed1f105e4e6bdebda
Antonio Lago <antonio.lago@gmail.com>
2022-09-05 10:57:44 +0200

    Consolidación de cambios

--- meteoPoliCode/meteoPoliCode.ino
+++ meteoPoliCode/meteoPoliCode.ino
@@ -5,7 +5,7 @@
   SDA --> GPIO21
   SCL (SCK) --> GPIO22
   PLUVIOMETRO --> GPIO27 (Pull-Down externo)
-  Anemómetro --> GPIO25 v=2,25*pulsos/Tiempo(s) (mph) = 3,62*pulsos/Tiempo(s) (km/h)
+  Anemómetro --> GPIO25 v=2,25*pulsos/Tiempo(s) (mph) = 3,62*pulsos/Tiempo(s) (km/h) . 1000 rph = 1 km/h
 */
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -21,6 +21,8 @@
 #define DIVISORTENSION 2
 #define ANEMOMETRO 25
 #define ANTIRREBOTE 15
+#define T_VENTO 3
+#define MUESTRAS 20
 //#define PROBANDO
 #if defined(PROBANDO)
   #define INTERVALO 20  // Segundos
@@ -32,11 +34,10 @@ Adafruit_BME280 bme; // I2C : D21 --> SDA  D22 --> SCL
 Adafruit_ADS1115 ads; // I2C Dir: 0x48
 WiFiMulti wifiMulti;
 
-volatile unsigned long pulsos=0;
-volatile unsigned long tiempoRebote=0;
-volatile unsigned long tiempoPrimerPulso=0;
-
+int pulsos=0;
 String serverName = "https://api.thingspeak.com/update?api_key=ZNPZIIHCRQ2P4VIX";
+
+//////////////////////////////////////////////////////////////////////
 
 void setup() {
   apagaWiFi();
@@ -49,49 +50,48 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(ANEMOMETRO), isr_pulso, FALLING);
   Serial.println("Estación meteorolóxica do IES Politécnico de Vigo");
   if (!bme.begin()) bmeError();
-    else Serial.print("BME 280 - SensorID : 0x"); Serial.println(bme.sensorID(),16);
+  else {Serial.print("BME 280 - SensorID : 0x"); Serial.println(bme.sensorID(),16);}
   configura();
   bme.takeForcedMeasurement();
   float temperatura = bme.readTemperature();
   float pre = bme.readPressure() / 100.0F;
   float presion = bme.seaLevelForAltitude(ELEVACION, pre);
-  float humedad=bme.readHumidity();
-  float vBat=mideTension();
-  int dir= dirViento();
-  float vel=0;
+  float humedad = bme.readHumidity();
+  float vBat = tensionBat();
+  int dir = dirVento();
+  float vel = velVento();
+  
  
   enciendeWiFi();  
-  //WiFi.begin(ssid, password); // Attempt to connect to wifi with our password
-  byte notConnectedCounter = 1;
-  wifiMulti.addAP("COCO", "12345678");
-  wifiMulti.addAP("AJOIR", "riquilante");
-  wifiMulti.addAP("AULA27", "");
-  wifiMulti.addAP("AULA26", "");
+  byte notConnectedCounter = 1;  
+  wifiMulti.addAP("humanidades", "p0l1t3cn1c0");
   wifiMulti.addAP("PARANINFO", "p0l1t3cn1c0");
-  wifiMulti.addAP("Sala Profes", "salapr0f3s");
+  //wifiMulti.addAP("COCO", "12345678");
+  //wifiMulti.addAP("AJOIR", "riquilante");
+  //wifiMulti.addAP("AULA27", "");
+  //wifiMulti.addAP("AULA26", "");
+  //wifiMulti.addAP("Sala Profes", "salapr0f3s");
   //wifiMulti.addAP("AJOIRr", "riquilante");
+  
   while (wifiMulti.run() != WL_CONNECTED) {
     Serial.print("Conectando a la wifi. Intento Nº : ");
     Serial.println(notConnectedCounter);
     delay(500);    
     notConnectedCounter++;
-    if(notConnectedCounter > 50) { // Resetear placa a los 50 intentos
+    if(notConnectedCounter > 20) { // Resetear placa a los 20 intentos
         Serial.println("Reinicio debido a la imposibilidad de conectar a la wifi.");
         ESP.restart();
     }
   } 
   Serial.println("");  Serial.println("WiFi connected: ");  Serial.println(WiFi.SSID());  Serial.println("IP address: ");  Serial.println(WiFi.localIP());
   
-  if (millis()<6000) delay(4000); // Damos tiempo para medir la velocidad del viento.
-  if (pulsos>0) {
-    vel = round(36.2 * 1000.0 * 10.0 * (pulsos * 1.0/((millis()-tiempoPrimerPulso)*1.0)))/10.0; // Redondeado a una cifra decimal
-  }
+ 
   Serial.println("Temperatura(ºC)\tPresión(hPa)\tHumedad\tV. Batería\tDirección\tVelocidad");  
-  Serial.print(temperatura);Serial.print("\t");
-  Serial.print(presion);Serial.print("\t");
+  Serial.print(temperatura);Serial.print("\t\t");
+  Serial.print(presion);Serial.print("\t\t");
   Serial.print(humedad);Serial.print("\t");
-  Serial.print(vBat);Serial.print("\t");
-  Serial.print(dir);Serial.print("\t");
+  Serial.print(vBat);Serial.print("\t\t");
+  Serial.print(dir);Serial.print("\t\t");
   Serial.println(vel); 
   if(WiFi.status()== WL_CONNECTED){ // Check to make sure wifi is still connected
     Serial.println("Enviando datos por Wifi");
@@ -112,9 +112,14 @@ void setup() {
   esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL, ESP_PD_OPTION_OFF);  
   esp_deep_sleep_start();
 }
+
+//////////////////////////////////////////////////////////////////////
+
 void loop() {   
          // Vacío. El código se ejecuta en Setup()  
 }
+
+//////////////////////////////////////////////////////////////////////
 
 void bmeError()
 {
@@ -122,15 +127,18 @@ void bmeError()
     Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n"); Serial.print("        ID of 0x60 represents a BME 280.\n");  Serial.print("        ID of 0x61 represents a BME 680.\n");    
 }
 
+//////////////////////////////////////////////////////////////////////
+
 void subeDatos(double temp, double pres, double hum, double vbat,int dir,double vel){
   HTTPClient http; // Initialize our HTTP client
-  String url = serverName + "&field1=" + temp + "&field2=" + pres + "&field3=" + hum + "&field4=" + vbat + "&field5=" + dir + "&field5=" + vel; 
+  String url = serverName + "&field1=" + temp + "&field2=" + pres + "&field3=" + hum + "&field4=" + vbat + "&field5=" + dir + "&field6=" + vel; 
   http.begin(url.c_str()); // Initialize our HTTP request
-  int httpResponseCode = http.GET(); // Send HTTP request
-  Serial.print("Respuesta HTTP: ");
-  Serial.println(httpResponseCode);
+  Serial.print("Respuesta HTTP:\t");
+  Serial.println(http.GET());
   http.end();
 }
+
+//////////////////////////////////////////////////////////////////////
 
 void configura() {
       bme.setSampling(Adafruit_BME280::MODE_FORCED,
@@ -139,37 +147,62 @@ void configura() {
                       Adafruit_BME280::SAMPLING_X1, // humidity
                       Adafruit_BME280::FILTER_OFF);
 }
-void apagaWiFi(){
+
+//////////////////////////////////////////////////////////////////////
+
+void apagaWiFi() {
     WiFi.disconnect(true);  // Desconecta de la red
     WiFi.mode(WIFI_OFF);    // Switch WiFi off
 }
-void enciendeWiFi(){
+
+//////////////////////////////////////////////////////////////////////
+
+void enciendeWiFi() {
     WiFi.disconnect(false);  // Reconnect the network
     WiFi.mode(WIFI_STA);
 }
-float mideTension(){
-  int acumulado = 0;
-  int i=1;
-  for (i=1;i<=20;i++) {
-    acumulado+= ads.readADC_SingleEnded(0);
-    delay(10);  
-    }
-  return(DIVISORTENSION*ads.computeVolts(acumulado/i));  
+
+//////////////////////////////////////////////////////////////////////
+
+float tensionBat() {
+  return(DIVISORTENSION*leeAdc(0));  
 }
-float dirViento(){
-  int acumulado = 0;
-  int i=1;
-  for (i=1;i<=20;i++) {
-    acumulado+= ads.readADC_SingleEnded(2);
-    delay(10);  
-    }
-  return(map((acumulado/i),0,32767,0,359));  
+
+//////////////////////////////////////////////////////////////////////
+
+int dirVento() {
+  return (round((leeAdc(2)/leeAdc(1))*359));
 }
-// Interrupción activada por pulso descendente en el hilo SPEED del anemómetro
-void isr_pulso() {
-  if((millis() - tiempoRebote) > ANTIRREBOTE ) { // 15 milisegundos mínimo antirrebote.
-    if (pulsos ==0) tiempoPrimerPulso=millis();
+
+//////////////////////////////////////////////////////////////////////
+
+float velVento() {
+  pulsos=0;
+  attachInterrupt(digitalPinToInterrupt(ANEMOMETRO), isr_pulso, FALLING);
+  delay(T_VENTO * 1000);
+  detachInterrupt(digitalPinToInterrupt(ANEMOMETRO));
+  return (round (36.2 * pulsos / T_VENTO) / 10.0);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+// Interrupción activada por flanco de bajada en el hilo SPEED del anemómetro
+void isr_pulso() {  
+  static unsigned long ultimoPulso=0;
+  if((millis() - ultimoPulso) > ANTIRREBOTE ) { // 15 milisegundos mínimo antirrebote.
     pulsos++;
-    tiempoRebote = millis();
+    ultimoPulso = millis();
   }
 } 
+
+//////////////////////////////////////////////////////////////////////
+
+float leeAdc(int entrada){
+  float acumulado = 0.0;
+  int i;    
+  for (i=1;i<=MUESTRAS;i++) {
+    acumulado += ads.computeVolts(ads.readADC_SingleEnded(entrada));
+    delay(10);
+  }
+  return (acumulado/MUESTRAS);  
+}
